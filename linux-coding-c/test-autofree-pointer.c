90993757d2f7f895ef14c0686e1a3833b59cb9b2
anzye <360845991@qq.com>
2023-04-13 10:29:54 +0800

    add c code examples

---
+++ linux-coding-c/test-autofree-pointer.c
@@ -0,0 +1,19 @@
+/**
+这行代码使用了GCC的一个特性，即attribute，它可以指定函数或变量的特定属性，以此来影响编译器的行为。在这个例子中，我们指定了一个cleanup属性，用于指定一个函数在变量离开作用域时自动调用以释放其内存。
+
+具体来说，这行代码定义了一个名为charp的char*类型的变量，并将其与cleanup属性相关联。当变量charp离开其作用域(例如，当其在函数中定义的时候，函数返回或者当一个代码块执行完毕时)时，编译器将自动调用一个名为free_charp的函数，用于释放charp所指向的内存。
+
+请注意，这需要确保在变量离开作用域之前，charp指向的内存不被重写或释放，否则可能会导致未定
+ */
+static void
+free_charp(char **ptr)
+{
+	free(*ptr);
+}
+
+#define autochar __attribute__((__cleanup__(free_charp))) char
+
+void test(){
+ autochar *charp = alloc(1, 1024);
+ // do something here
+}
---
+++ linux-coding-c/test-timeout-fgets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <signal.h>
+#include <setjmp.h>
+
+sigjmp_buf env;
+void tfgets_handler(int sig)
+{
+  //signal(SIGALRM, SIG_DFL);
+  signal(SIGALRM, SIG_IGN);
+  siglongjmp(env, 1);
+}
+
+char *tfgets(char *buf, int bufsize, FILE *stream)
+{
+  static const int TimeLimitSecs = 5;
+  signal(SIGALRM, tfgets_handler)
+  alarm(TimeLimitSecs);
+  int rc = sigsetjmp(env, 1);
+  if(rc == 0) {
+    char *ret = fgets(buf, bufsize, stream);
+    signal(SIGALRM, SIG_IGN);
+    return ret;
+  }
+  else {
+    return NULL; //alarm，time out
+  }
+}
